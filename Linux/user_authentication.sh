8bc8b8ab2fc30b990863abec9f45a920d17194b8
Olatunji Damilare Emmanuel <dammy4did@gmail.com>
2023-08-09 21:57:36 +0200

    Last Update

--- Linux/user_authentication.sh
+++ Linux/user_authentication.sh
@@ -26,8 +26,25 @@ register_credentials() {
     # Insert code to register add the created user to a file called credentials.txt
     echo "----Registration----"
     read -p "Enter Username: " username_reg
-    read -s -p "Enter Password" password_reg
-    echo "" # Add a newline after reading the password
+    # Read password and ensure it's not empty
+    while true; do
+        read -s -p "Enter Password: " password_reg
+        echo ""  # Add a newline after reading the password
+        read -s -p "Confirm Password " password_reg2
+        echo "" # Add a new line after reading the password
+
+        if [ -z "$password_reg" ]; then
+            echo "Password cannot be empty. Please try again."
+        elif [ "$password_reg" != "$password_reg2" ]; then
+            echo "Password not matching, please try again..."
+            read -s -p "Enter Password Again: " password_reg
+            echo ""  # Add a newline after reading the password
+            read -s -p "Confirm Password Again: " password_reg2
+            echo "" # Add a new line after reading the password
+        else
+            break
+        fi
+    done
     echo -n "Enter Full Name: "
     read full_name
     role=normal
@@ -36,35 +53,95 @@ register_credentials() {
     if grep -q "^$username_reg:" "$credentials_file"; then
         echo "Error: User '$username_reg' already exists."
     else
+    # Hash the input password
     local refined_password=$(hash_password "$password")
+    # Append the collected records into the credential file.
         echo "$username_reg:$refined_password:$full_name:$role:$login_status" >> "$credentials_file"
         echo ""
         echo -e "User '$username_reg' Registration successful. You can now log in.\n"
     fi
 }
 
-# Function to verify credentials and privileges
-verify_credentials() {
-    
-    local user_info=$(grep "^$user:" "$credentials_file" | cut -d ':' -f 2-)
+# Login Function
+login_user() {
+echo "Welcome to the login page. Please enter your username and password."
+    get_credentials
+   
+    # Check if the user exists...
+    local user_info=$(grep "^$username:" "$credentials_file" | cut -d ':' -f 2-)
     if [[ -n "$user_info" ]]; then
         local stored_pass=$(echo "$user_info" | cut -d ':' -f 1)
         local salt=$(echo "$user_info" | cut -d ':' -f 2)
         local hashed_pass=$(echo -n "$pass$salt" | sha256sum | awk '{print $1}')
-        
-        # check whether the credentials provided are accurate 
-      -  	# Write here
-            # On successful login remember to update the credentials.txt file for login status to 1
- 	# if the credentials provided are correct, check the role. If the role is admin, call the admin menu
- 		# write here
- 	# Otherwise call user menu
- 		# Write your code here
+        local login_status=$(echo "$user_info" | cut -d ':' -f 5)
+        local status=$(grep "^$username:" "$credentials_file")
+
+        # Change the news state to 1
+        if [[ "$login_status"=="0" ]]; then
+            echo "You're welcome $username... You're the only active user for now..."
+            new_status=$(echo "$status" | awk 'BEGIN{FS=OFS=":"} {$6="1"; print}')
+            sed -i "s~$status~$new_status~" "$credentials_file"
+        fi
+
+        # Extract the value of the 'role' variable
+        local user_role=$(echo "$user_info" | cut -d ':' -f 4)
+
+        # Normal User - Display Dashboard
+        if [ "$user_role" == "normal" ]; then
+            user_menu
+        # Admin User - Display Dashboard
+        elif [ "$user_role" == "admin" ]; then
+            admin_menu
+        else
+            salesperson_menu
+        fi
+
+    else
+        echo -e "Unsuccessful login. Incorrect username or password. Please try again.\n" 
     fi
-    echo -e "Unsuccessful login. Incorrect username or password. Please try again.\n"
-    return 1
 }
 
-# Function for the admin menu
+# Logout Function
+logout_user() {
+    local status=$(grep "^$username:" "$credentials_file")
+    local login_status=$(echo "$status" | awk -F':' '{print $6}')
+
+    if [[ "$login_status" == "1" ]]; then
+        new_status=$(echo "$status" | awk 'BEGIN{FS=OFS=":"} {$6="0"; print}')
+        sed -i "s~$status~$new_status~" "$credentials_file"
+        echo -e "\nLogout successful..." 
+        exit 0  
+    fi
+}
+
+# Exit Function
+exit_user(){
+    echo "Exiting User System. Goodbye!"
+    exit 0             
+}
+
+
+# Delete Function
+delete_user(){
+        echo -n "Enter the username to delete: "
+        read del_username
+
+        # Check if the username exists in the file
+        if grep -q "^$del_username:" "$credentials_file"; then
+
+            # Delete the line containing the username from the file
+            sed -i "/^$del_username:/d" "$credentials_file"
+
+            echo "User $del_username deleted from $credentials_file"
+            admin_menu
+            
+        else
+            echo "User $del_username not found in $credentials_file"
+        fi
+}
+
+# ------------- Different Users Categories and Privileges -------------
+# Admin Menu Function
 admin_menu() {
 
         # Function to register new credentials
@@ -99,42 +176,11 @@ admin_menu() {
             echo ""
             echo -e "User '$username_reg_2' Registration successful. You can now log in.\n"
         fi
-    }
+
+        
+}
     # write a logic that allows a user logged in as admin to create an account. 
     # This function must allow the logged in user to create many users. 
-    
-    delete_user(){
-        echo -n "Enter the username to delete: "
-        read username
-
-        # Check if the file exists
-        if [ ! -f "$credentials_file" ]; then
-            echo "File not found"
-            return 1
-        fi
-
-        # Check if the username exists in the file
-        if grep -q "^$username:" "$credentials_file"; then
-
-            # Delete the line containing the username from the file
-            sed -i "/^$username:/d" "$credentials_file"
-
-            echo "User $username deleted from $credentials_file"
-            
-        else
-            echo "User $username not found in $credentials_file"
-        fi
-    }
-
-    # Write your code here  
-    admin_display_menu() {
-                echo "Welcome to the admin dashboard."
-                echo "1. Create an Account"
-                echo "2. Delete an Account"
-                echo "3. Logout"
-                echo "4. Exit"
-                echo -n "Enter your choice: " 
-            }
 
             while true; do
                 admin_display_menu
@@ -149,10 +195,6 @@ admin_menu() {
                             ;;
                     3)      
                             logout_user
-                            break
-                            ;;
-                    4) 
-                            exit_user
                             ;;
                     *)
                         echo "Invalid choice. Please try again."
@@ -161,8 +203,15 @@ admin_menu() {
 
                 echo ""
             done 
+}
 
-            # admin_display_menu
+# Admin Display Menu  
+admin_display_menu() {
+    echo "Welcome to the admin dashboard."
+    echo "1. Create an Account"
+    echo "2. Delete an Account"
+    echo "3. Logout"
+    echo -n "Enter your choice: " 
 }
 
 # Function for the user menu
@@ -211,8 +260,8 @@ salesperson_menu() {
     esac
 }
 
-# Main script execution starts here
-# Function to display the menu
+
+# The general display menu 
 display_menu() {
     echo "Welcome to the authentication system."
     echo "1. Login"
@@ -220,91 +269,22 @@ display_menu() {
     echo "3. Exit"
     echo -n "Enter your choice: "
 }
+
 # write a script that allows a system user to login, register, logout and exit from the system
-
-# Function to handle user login
-login_user() {
-    echo "Welcome to the login page. Please enter your username and password."
-    get_credentials
-   
-    # Check if the user exists...
-    local user_info=$(grep "^$username:" "$credentials_file" | cut -d ':' -f 2-)
-    if [[ -n "$user_info" ]]; then
-        local stored_pass=$(echo "$user_info" | cut -d ':' -f 1)
-        local salt=$(echo "$user_info" | cut -d ':' -f 2)
-        
-        local hashed_pass=$(echo -n "$pass$salt" | sha256sum | awk '{print $1}')
-        local login_status=$(echo "$user_info" | cut -d ':' -f 5)
-        
-        local status=$(grep "^$username:" "$credentials_file")
-
-        # Change the news state to 1
-        if [[ "$login_status"=="0" ]]; then
-            echo "You're welcome $username... You're the only active user for now..."
-            new_status=$(echo "$status" | awk 'BEGIN{FS=OFS=":"} {$6="1"; print}')
-            sed -i "s~$status~$new_status~" "$credentials_file"
-        fi
-
-        # Extract the value of the 'role' variable
-        local user_role=$(echo "$user_info" | cut -d ':' -f 4)
-
-        # Normal User - Display Dashboard
-        if [ "$user_role" == "normal" ]; then
-            user_menu
-        # Admin User - Display Dashboard
-        elif [ "$user_role" == "admin" ]; then
-            admin_menu
-        else
-            salesperson_menu
-        fi
-
-    else
-        echo -e "Unsuccessful login. Incorrect username or password. Please try again.\n" 
-    fi
-        
-}
-
-# Function to handle user logout
-logout_user() {
-    local status=$(grep "^$username:" "$credentials_file")
-    local login_status=$(echo "$status" | awk -F':' '{print $6}')
-    echo $login_status
-
-    if [[ "$login_status" == "1" ]]; then
-        echo -e "\nUpdating login status to 0"
-        new_status=$(echo "$status" | awk 'BEGIN{FS=OFS=":"} {$6="0"; print}')
-        sed -i "s~$status~$new_status~" "$credentials_file"
-        echo -e "\nLogout successful..."
-    fi
-    return
-}
-
-
-exit_user(){
-    # Change the news state to 0
-        local status=$(grep "^$username:" "$credentials_file")
-        if [[ "$login_status"=="1" ]]; then
-            new_status=$(echo "$status" | awk 'BEGIN{FS=OFS=":"} {$6="0"; print}')
-            sed -i "s~$status~$new_status~" "$credentials_file"
-            echo "Exiting User System. Goodbye!"
-            exit 0   
-        fi
-          
-}
-
 while true; do
     display_menu
     read choice
 
     case "$choice" in
         1)
-            login_user
-            ;;
+                login_user
+                ;;
         2)
-            register_credentials
-            ;;
+                register_credentials
+                ;;
         3)
             exit_user
+            break
             ;;
         *)
             echo "Invalid choice. Please try again."
